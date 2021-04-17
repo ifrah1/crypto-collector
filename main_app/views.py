b747600356d6ef21304c1ce66593b97a87972743
ifrah1 <arifrah125@gmail.com>
2021-04-17 14:42:20 -0400

    created admin users for admin page

--- main_app/views.py
+++ main_app/views.py
@@ -2,18 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Crypto
 
-class Crypto:  
-    def __init__(self, name, price, description, amount):
-        self.name = name
-        self.price = price
-        self.description = description
-        self.amount = amount
-
-cryptos = [
-  Crypto('Bitcoin', 60000, 'OG bitty', 3),
-  Crypto('Litecoin', 300, 'fork of bitty but is litty', 500),
-]
 
 # Create your views here.
 
@@ -25,4 +15,5 @@ def about(request):
     return render(request, 'about.html')
 
 def cryptos_index(request):
+    cryptos = Crypto.objects.all()
     return render(request, 'cryptos/index.html', { 'cryptos': cryptos })
