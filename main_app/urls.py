bc4a245d400a3d6b0ef1816ceba371ac1104d799
ifrah1 <arifrah125@gmail.com>
2021-04-17 14:28:41 -0400

    finished showing crypto class list

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,11 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-#     # route for cats index
-#     path('cats/', views.cats_index, name='index'),
-#     path('cats/<int:cat_id>/', views.cats_detail, name='detail'),
-#     # new route used to show a form and create a cat
-#     path('cats/create/', views.CatCreate.as_view(), name='cats_create'),
-#     path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cats_update'),
-#     path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cats_delete'),
+     path('cryptos/', views.cryptos_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
+class Crypto:  
+    def __init__(self, name, price, description, amount):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.amount = amount
+
+cryptos = [
+  Crypto('Bitcoin', 60000, 'OG bitty', 3),
+  Crypto('Litecoin', 300, 'fork of bitty but is litty', 500),
+]
+
 # Create your views here.
 
 # Define the home view
@@ -10,3 +23,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def cryptos_index(request):
+    return render(request, 'cryptos/index.html', { 'cryptos': cryptos })
