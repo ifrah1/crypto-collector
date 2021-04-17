4a510b86440348b5228dc8454b29ba1ce26abd5d
ifrah1 <arifrah125@gmail.com>
2021-04-17 14:36:20 -0400

    added crypto model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+# Import the reverse function
+from django.urls import reverse
+
 # Create your models here.
+class Crypto(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField(max_length=250)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return self.name
