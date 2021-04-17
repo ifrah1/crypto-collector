c7f8d4ce8eb3a56ce89c86252d2e8fa0d487209e
ifrah1 <arifrah125@gmail.com>
2021-04-17 14:55:50 -0400

    finished model lab

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-     path('cryptos/', views.cryptos_index, name='index'),
+    path('cryptos/', views.cryptos_index, name='index'),
+    path('cryptos/<int:crypto_id>/', views.cryptos_detail, name='detail'),
 ]
